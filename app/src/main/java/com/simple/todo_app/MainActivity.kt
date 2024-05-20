318a32699556c82e727f7f059c91fc0d7faf8536
Abdul Basit <basitbhattee@gmail.com>
2024-05-20 12:05:38 +0500

    Todo App Commit.

---
+++ app/src/main/java/com/simple/todo_app/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.simple.todo_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import com.simple.todo_app.ui.theme.ToDoAppTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToDoAppTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary
+                            ),
+
+                            title = { Text(text = "Todo App") })
+                    }
+                ) { innerPadding ->
+
+                    Column (
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
